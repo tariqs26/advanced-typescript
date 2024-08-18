522696f291306b1d363fc6818cd5c465950cbfce
Saad Tariq <saadtariq.dev@outlook.com>
2024-08-18 15:16:42 -0400

    refactor: add support for deeply nested readonly and partial types and remove record extensions

--- src/mapped-types.ts
+++ src/mapped-types.ts
@@ -1,19 +1,31 @@
-export type Pick<
-  T extends Record<PropertyKey, unknown>,
-  Keep extends keyof T
-> = {
+export type Pick<T, Keep extends keyof T> = {
   [Key in keyof T as Key extends Keep ? Key : never]: T[Key]
 }
 
-export type Omit<
-  T extends Record<PropertyKey, unknown>,
-  Remove extends keyof T
-> = {
+export type Omit<T, Remove extends keyof T> = {
   [Key in keyof T as Key extends Remove ? never : Key]: T[Key]
 }
 
-export type Partial<T extends Record<PropertyKey, unknown>> = {
-  [Key in keyof T]?: T[Key]
+export type Partial<T, Deep extends boolean = false> = {
+  [Key in keyof T]?: Deep extends false
+    ? T[Key]
+    : T[Key] extends object
+    ? Partial<T[Key], Deep>
+    : T[Key]
+}
+
+export type Readonly<T, Deep extends boolean = false> = {
+  readonly [Key in keyof T]: Deep extends false
+    ? T[Key]
+    : T[Key] extends object
+    ? Readonly<T[Key], Deep>
+    : T[Key]
+}
+
+export type Merge<F, S> = {
+  [Key in keyof F | keyof S]: Key extends keyof S
+    ? S[Key]
+    : F[Key extends keyof F ? Key : never]
 }
 
 type Events = {
@@ -22,7 +34,7 @@ type Events = {
   keydown: KeyboardEvent
 }
 
-type On<T extends object> = {
+type On<T> = {
   [Key in keyof T as Key extends string ? `on${Capitalize<Key>}` : never]?: (
     e: T[Key]
   ) => void
@@ -37,10 +49,4 @@ const actions: On<Events> = {
     e.key
     // ...
   },
-}
-
-export type Merge<F, S> = {
-  [Key in keyof F | keyof S]: Key extends keyof S
-    ? S[Key]
-    : F[Key extends keyof F ? Key : never]
 }
