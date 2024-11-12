0f4a201e4fae33556f0b8790e5d42ef64865e4b8
Saad Tariq <saadtariq.dev@outlook.com>
2024-11-12 01:45:10 -0500

    feat: add custom console.log declaration with typed format specifiers support

---
+++ src/formatted-log.ts
@@ -0,0 +1,34 @@
+type FormatSpecifiers = { [key in "d" | "i"]: number } & { s: string } & {
+  [key in "o" | "O"]: object
+}
+
+type ExtractFormatSpecifiers<
+  Str,
+  Args extends any[] = []
+> = Str extends `${infer _}%${infer Specifier}${infer Rest}`
+  ? Specifier extends keyof FormatSpecifiers
+    ? ExtractFormatSpecifiers<Rest, [...Args, FormatSpecifiers[Specifier]]>
+    : ExtractFormatSpecifiers<Rest, Args>
+  : Args
+
+type MessageValueTypes =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | symbol
+  | bigint
+  | object
+
+declare var console: Omit<Console, "log"> & {
+  log: <T extends MessageValueTypes>(
+    message?: T,
+    ...args: T extends string
+      ? [...ExtractFormatSpecifiers<T>, ...unknown[]]
+      : unknown[]
+  ) => void
+}
+
+// console.log("Hello %s", 1) // error
+console.log("Hello %s %d", "world", 20) // ok
