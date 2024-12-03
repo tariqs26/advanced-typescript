2c831d72cd9e113a5b97113fa583df0c7805e3e4
Saad Tariq <saadtariq.dev@outlook.com>
2024-12-03 14:52:54 -0500

    refactor: simplify pick and omit types and improve readability

--- src/mapped-types/index.ts
+++ src/mapped-types/index.ts
@@ -1,29 +1,29 @@
 export type Pick<T, Keep extends keyof T> = {
-    [Key in keyof T as Key extends Keep ? Key : never]: T[Key]
-  }
-  
-  export type Omit<T, Remove extends keyof T> = {
-    [Key in keyof T as Key extends Remove ? never : Key]: T[Key]
-  }
-  
-  export type Partial<T, Deep extends boolean = false> = {
-    [Key in keyof T]?: Deep extends false
-      ? T[Key]
-      : T[Key] extends object
-      ? Partial<T[Key], Deep>
-      : T[Key]
-  }
-  
-  export type Readonly<T, Deep extends boolean = false> = {
-    readonly [Key in keyof T]: Deep extends false
-      ? T[Key]
-      : T[Key] extends object
-      ? Readonly<T[Key], Deep>
-      : T[Key]
-  }
-  
-  export type Merge<F, S> = {
-    [Key in keyof F | keyof S]: Key extends keyof S
-      ? S[Key]
-      : F[Key extends keyof F ? Key : never]
-  }
+  [Key in Keep]: T[Key]
+}
+
+export type Omit<T, Remove extends keyof T> = {
+  [Key in Exclude<keyof T, Remove>]: T[Key]
+}
+
+export type Partial<T, Deep extends boolean = false> = {
+  [Key in keyof T]?: Deep extends false
+    ? T[Key]
+    : T[Key] extends object
+    ? Partial<T[Key], Deep>
+    : T[Key]
+}
+
+export type Readonly<T, Deep extends boolean = false> = {
+  readonly [Key in keyof T]: Deep extends false
+    ? T[Key]
+    : T[Key] extends object
+    ? Readonly<T[Key], Deep>
+    : T[Key]
+}
+
+export type Merge<F, S> = {
+  [Key in keyof F | keyof S]: Key extends keyof S
+    ? S[Key]
+    : F[Key extends keyof F ? Key : never]
+}
